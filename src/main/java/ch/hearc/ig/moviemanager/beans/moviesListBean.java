2e751f29073a525fbb9995367fb979d96e0f7e61
DraftDario <dario.mosca@he-arc.ch>
2016-12-08 13:12:30 +0100

    AddJavadocComment

--- src/main/java/ch/hearc/ig/moviemanager/beans/moviesListBean.java
+++ src/main/java/ch/hearc/ig/moviemanager/beans/moviesListBean.java
@@ -18,7 +18,7 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 
 /**
- *
+ * This bean is for moviesList.xhtml
  * @author dario.mosca
  */
 @Named(value = "moviesList")
@@ -38,26 +38,50 @@ public class moviesListBean implements Serializable {
     public moviesListBean() {
     }
 
+    /**
+     * Getter for movie
+     * @return the movie
+     */
     public Movie getMovie() {
         return movie;
     }
 
+    /**
+     * Setter for movie
+     * @param movie parameter to set
+     */
     public void setMovie(Movie movie) {
         this.movie = movie;
     }
 
+    /**
+     * Getter for personID
+     * @return the person's ID
+     */
     public Long getPersonID() {
         return personID;
     }
 
+    /**
+     * Setter for personID
+     * @param personID the parameter to set
+     */
     public void setPersonID(Long personID) {
         this.personID = personID;
     }
 
+    /**
+     * Getter for person
+     * @return the person
+     */
     public Person getPerson() {
         return person;
     }
 
+    /**
+     * Setter for person
+     * @param person the parameter to set
+     */
     public void setPerson(Person person) {
         this.person = person;
     }
@@ -70,10 +94,20 @@ public class moviesListBean implements Serializable {
         person = services.getPersonWithId(personID);
     }
 
+    /**
+     * Delete a movie from the movieList for the current person
+     * @param movie the movie to delete
+     * @throws NullParameterException exception if null parameter
+     * @throws InvalidParameterException exception if parameter not valid
+     */
     public void deleteMovie(Movie movie) throws NullParameterException, InvalidParameterException {
         services.removeMovieFromPerson(person, movie);
     }
 
+    /**
+     * Method to get all the movies that are not in the current person movies' list
+     * @return the list of other movies
+     */
     public List getOtherMovies() {
         List<Movie> movies = services.getMoviesList();
         List<Movie> personMovies = person.getMovies();
@@ -90,6 +124,11 @@ public class moviesListBean implements Serializable {
         return movies;
     }
 
+    /**
+     * Method to add a movie in the current person movies' list
+     * @throws NullParameterException if parameter null
+     * @throws UniqueException if not unique
+     */
     public void addMovie() throws NullParameterException, UniqueException {
         if (movie != null) {
             person.addMovie(movie);
